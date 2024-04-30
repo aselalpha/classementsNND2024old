d165fb8f8eb45af15e40cff38e39e6a63c7b3288
Asellus Alpha <mc.firealpha@gmail.com>
2024-05-01 00:44:07 +0200

    test collect_teams

--- models/team.py
+++ models/team.py
@@ -1,2 +1,22 @@
+import pandas as pd
+
+
 class Team:
-    pass
+
+    def __init__(self, puce: int, dossard: int, ent: bool, mixite: str, team_name: str, concs_list: list[tuple[str,str]], contact: str):
+        self.puce = puce
+        self.dossard = dossard
+        self.team_name = team_name
+        self.ent = ent
+        self.mixite = mixite
+        self.concs_list = concs_list
+        self.contact = contact
+
+    def __repr__(self):
+        return f'Team({self.puce}, n°{self.dossard}, {self.team_name})'
+    
+    def __eq__(self, other):
+        """Compare deux objets Team, qui sont considérées comme identiques si elles ont même nom, même numéro de puce, même numéro de dossard."""
+        if isinstance(self, other.__class__):
+            return self.puce == other.puce and self.dossard == other.dossard and self.team_name == other.team_name
+        return False
--- test/test.py
+++ test/test.py
@@ -1,14 +1,21 @@
 import unittest
 import pandas as pd
 
-from datacollection.teamscollection import get_size_of_teams
+from datacollection.teamscollection import get_size_of_teams, collect_teams
+
+from models.team import Team
 
 
 class TestTeamsCollection(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.teams_df: pd.DataFrame = pd.read_excel('JOURNEES/J1/teams.xlsx').dropna(how='all')
-
-
     def test_get_size_of_teams(self):
-        self.assertEqual(get_size_of_teams(self.teams_df), 2)
+        teams_df: pd.DataFrame = pd.read_excel('JOURNEES/J1/teams.xlsx').dropna(how='all')
+        self.assertEqual(get_size_of_teams(teams_df), 2)
+
+    
+    def test_collect_teams(self):
+        teams_excel_path: str = 'JOURNEES/J1/teams.xlsx'
+        teams_list: list[Team] = collect_teams(teams_excel_path)
+
+        self.assertEqual(len(teams_list), 142)
+        self.assertEqual(teams_list[2], Team(1915532, 3, False, 'mixte', 'Les pâ€™tits dangereux', [('Han', 'Vanessa'), ('Fagard', 'LÃ©o')], 'vanessa.han@student-cs.fr'))
